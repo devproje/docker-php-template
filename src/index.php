2a50d7b4526ec7504ec9b4ec64d36d16c6d85025
Project_IO <me@projecttl.net>
2023-11-20 17:35:43 +0900

    rm: .idea dir

--- src/index.php
+++ src/index.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8"/>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="stylesheet" href="/static/css/style.css"/>
-        <title>PHP Homework</title>
-    </head>
-    <body>
-        <div class="container">
-            <h1>PHP Homework</h1>
-            <div class="list">
-                <?php dir_list() ?>
-            </div>
-        </div>
-    </body>
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="/static/css/style.css"/>
+    <title>PHP Homework</title>
+</head>
+<body>
+<div class="container">
+    <h1>PHP Homework</h1>
+    <div class="list">
+        <?php dir_list() ?>
+    </div>
+</div>
+</body>
 </html>
 
 <?php
@@ -41,6 +41,6 @@ function scan(string $dir) {
         }
 
         $href = "/$dir/$sel";
-        echo "<a class='item' href='$href'><span id='name'>{$sel}</span></a><br/>";
+        echo "<a class='item' href='$href'><span id='name'>$sel</span></a><br/>";
     }
 }
